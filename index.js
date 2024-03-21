6dcefaa32b81affcbfe6f423b665dbaea8ec2213
Rahul Bhardwaj <147973917+rahulxbhardwaj@users.noreply.github.com>
2024-03-21 06:18:39 +0000

    add more random fact with another file

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 import express from "express";
 const app = express();
 const port = 8080;
+import randomFact from "./randomFact.js"
 
-app.use(express.json());
+// app.use(express.json());
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
 app.get('/fact' , (req,res) => {
-
+  
   const jsonData = {
-   fact : "This is a Random Fact"
+   fact : randomFact()
   };
   res.send(jsonData);
 })
